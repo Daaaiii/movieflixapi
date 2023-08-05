1e31a4461c37c00b494979154d6752c69f40d47a
Daiane Bolzan <dainha85@gmail.com>
2023-08-05 16:12:07 -0300

    adicionada a correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,14 @@ app.get("/movies", async (_, res) => {
 app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
     try {
+
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: { equals: title, mode: "insensitive" } }
+        });
+        if (movieWithSameTitle) {
+            return res.status(409).send({ message: "Já existe um filme cadastrado com esse título." });
+        }
+
         await prisma.movie.create({
             data: {
                 title,
@@ -30,7 +38,7 @@ app.post("/movies", async (req, res) => {
             },
         });
     } catch (error) {
-        return res.status(500).send({ message: "Falha ao cadastrar o filme" })
+        return res.status(500).send({ message: "Falha ao cadastrar o filme" });
     }
     res.status(201).send();
 });
